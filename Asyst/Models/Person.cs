5f6ab1aea80e6b552ba0957f13d322b8aa015eea
BMJAKARTA\vsimamora <quedeve@gmail.com>
2023-10-17 17:39:58 +0700

    update error message

--- Asyst/Models/Person.cs
+++ Asyst/Models/Person.cs
@@ -12,7 +12,13 @@ namespace Asyst.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 100 characters.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email address.")]
+        [StringLength(100, ErrorMessage = "Email must be no more than 100 characters.")]
         public string Email { get; set; }
     }
 }
